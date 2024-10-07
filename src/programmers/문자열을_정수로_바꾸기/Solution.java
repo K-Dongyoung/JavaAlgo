17aeef55bfa5c248dd8b4e796b48a11a06af1429
K.Dongyoung <doyokim75@gmail.com>
2024-10-07 14:25:47 +0900

    241007 문자열을 정수로 바꾸기 programmers

---
+++ src/programmers/문자열을_정수로_바꾸기/Solution.java
@@ -0,0 +1,26 @@
+package programmers.문자열을_정수로_바꾸기;
+
+public class Solution {
+
+    public int solution1(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public int solution(String s) {
+        int sign = 1;
+        int answer = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                answer = answer * 10 + c - 48;
+            } else if (c == '-') {
+                sign = -1;
+            }
+        }
+        return answer * sign;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println("solution.solution(\"-1234\") = " + solution.solution("-1234"));
+    }
+}
