ada27c92644db709ce7c227d45d5dff93fe4c74c
K.Dongyoung <doyokim75@gmail.com>
2024-10-10 14:12:18 +0900

    241010 평균 구하기 programmers

---
+++ src/programmers/평균_구하기/Solution.java
@@ -0,0 +1,17 @@
+package programmers.평균_구하기;
+
+public class Solution {
+    public double solution(int[] arr) {
+        double sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        double average = solution.solution(new int[]{1, 2, 3, 4});
+        System.out.println("average = " + average);
+    }
+}
