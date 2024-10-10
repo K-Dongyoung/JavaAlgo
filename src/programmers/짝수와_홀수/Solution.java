d5f7a0599f294c4d7b86ae8020b2c2decc849a50
K.Dongyoung <doyokim75@gmail.com>
2024-10-10 14:28:09 +0900

    241010 짝수와 홀수 programmers

---
+++ src/programmers/짝수와_홀수/Solution.java
@@ -0,0 +1,11 @@
+package programmers.짝수와_홀수;
+
+public class Solution {
+
+    public String solution(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
+    }
+}
