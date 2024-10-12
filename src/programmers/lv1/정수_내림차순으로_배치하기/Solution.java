2a3fabadaaf422d6fb4d688ae5cc8bd27795162b
K.Dongyoung <doyokim75@gmail.com>
2024-10-12 21:53:52 +0900

    241012 정수_내림차순으로_배치하기 programmers

--- src/programmers/lv1/정수_내림차순으로_배치하기/Solution.java
+++ src/programmers/lv1/정수_내림차순으로_배치하기/Solution.java
@@ -1,12 +1,12 @@
 package programmers.lv1.정수_내림차순으로_배치하기;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.Collections;
 
 
 public class Solution {
-    public long solution(long n) {
+    public long solution1(long n) {
         char[] charArray = String.valueOf(n).toCharArray();
         String[] characters = new String[charArray.length];
         for (int i = 0; i < charArray.length; i++) {
@@ -16,8 +16,14 @@ public class Solution {
         return Long.parseLong(String.join("", characters));
     }
 
+    public long solution2(long n) {
+        String[] characters = Long.toString(n).split("");
+        Arrays.sort(characters, Collections.reverseOrder());
+        return Long.parseLong(String.join("", characters));
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println("solution.solution(118372) = " + solution.solution(118372));
+        System.out.println("solution.solution(118372) = " + solution.solution2(118372));
     }
 }
