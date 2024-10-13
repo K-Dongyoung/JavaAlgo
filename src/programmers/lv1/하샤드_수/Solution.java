b07e153d700f5a313d5195d4762a91bcc201d3c4
K.Dongyoung <doyokim75@gmail.com>
2024-10-13 10:40:28 +0900

    241013 하샤드_수 programmers

--- src/programmers/lv1/하샤드_수/Solution.java
+++ src/programmers/lv1/하샤드_수/Solution.java
@@ -3,17 +3,22 @@ package programmers.lv1.하샤드_수;
 import java.util.Arrays;
 
 public class Solution {
-    public boolean solution(int x) {
+    public boolean solution1(int x) {
         String[] nums = String.valueOf(x).split("");
         int sum = Arrays.stream(nums).mapToInt(Integer::valueOf).sum();
         return x % sum == 0;
     }
 
+    public boolean solution2(int x) {
+        int sum = String.valueOf(x).chars().map(n -> n - '0').sum();
+        return x % sum == 0;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        boolean solution1 = solution.solution(10);
+        boolean solution1 = solution.solution2(10);
         System.out.println("solution1 = " + solution1);
-        boolean solution2 = solution.solution(11);
+        boolean solution2 = solution.solution2(11);
         System.out.println("solution1 = " + solution2);
     }
 }
