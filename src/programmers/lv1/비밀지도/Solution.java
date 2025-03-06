9eae90b0bc3fb38cf93be5b93136b3bbd9f3bb0d
K.Dongyoung <doyokim75@gmail.com>
2025-03-06 17:17:15 +0900

    250306 [1차] 비밀지도 programmers

--- src/programmers/lv1/비밀지도/Solution.java
+++ src/programmers/lv1/비밀지도/Solution.java
@@ -3,10 +3,11 @@ package programmers.lv1.비밀지도;
 import java.util.Arrays;
 
 public class Solution {
-    public String[] failed_solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution1(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for(int i = 0; i < n; i++) {
             String b = Integer.toBinaryString(arr1[i] | arr2[i]);
+            b = "0".repeat(n - b.length()) + b;
             StringBuilder sb = new StringBuilder();
             for(char c : b.toCharArray()) {
                 sb.append((c == '1') ? "#" : " ");
@@ -16,7 +17,7 @@ public class Solution {
         return answer;
     }
 
-    public String[] solution(int n, int[] arr1, int[] arr2) {
+    public String[] solution2(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for(int i = 0; i < n; i++) {
             StringBuilder sb = new StringBuilder();
@@ -40,7 +41,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28})));
-        System.out.println(Arrays.toString(s.solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10})));
+        System.out.println(Arrays.toString(s.solution1(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28})));
+        System.out.println(Arrays.toString(s.solution1(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10})));
     }
 }
