e40fe833c1718edf228cdf4067b40557c147ed48
K.Dongyoung <doyokim75@gmail.com>
2025-03-29 10:20:28 +0900

    250329 B4_24263 백준

---
+++ src/baekjoon/bronze/B4_24263/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.bronze.B4_24263;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+        System.out.println(1);
+    }
+}
