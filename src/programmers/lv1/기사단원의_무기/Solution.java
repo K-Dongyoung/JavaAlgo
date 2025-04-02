60726d59d277e8ef131a80fe739b2d60c97a82e5
K.Dongyoung <doyokim75@gmail.com>
2025-04-02 12:59:23 +0900

    250402 기사단원의_무기 programmers

---
+++ src/programmers/lv1/기사단원의_무기/Solution.java
@@ -0,0 +1,22 @@
+package programmers.lv1.기사단원의_무기;
+
+public class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 1;
+        for (int i = 2; i <= number; i++) {
+            int count = 0;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) count += 2;
+                if (j == Math.sqrt(i)) count --;
+            }
+            answer += count > limit ? power : count;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(5, 3, 2));
+        System.out.println(s.solution(10, 3, 2));
+    }
+}
