f9426469acd945ec1a91d94607235b03b13f81d3
K.Dongyoung <doyokim75@gmail.com>
2025-05-01 17:32:46 +0900

    250501 프로세스 programmers

---
+++ src/programmers/lv2/프로세스/Solution2.java
@@ -0,0 +1,30 @@
+package programmers.lv2.프로세스;
+
+import java.util.Arrays;
+
+public class Solution2 {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int[] orderedList = new int[priorities.length];
+        System.arraycopy(priorities, 0, orderedList, 0, priorities.length);
+        Arrays.sort(orderedList);
+        int order = priorities.length - 1;
+        for (int i = 0;; i++) {
+            int index = i % priorities.length;
+            if (priorities[index] == 0) continue;
+            if (priorities[index] == orderedList[order]) {
+                priorities[index] = 0;
+                answer++;
+                order--;
+                if (index == location) return answer;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+        System.out.println(s.solution(new int[]{2, 1, 3, 2}, 2));
+        System.out.println(s.solution(new int[]{1, 1, 9, 1, 1, 1}, 0));
+        System.out.println(s.solution(new int[]{3, 4, 1, 2, 3, 5, 6, 2, 1, 3, 3, 7, 2}, 4));
+    }
+}
