2c21102343f62341d538769952621b9f837e5a6b
K.Dongyoung <doyokim75@gmail.com>
2025-09-12 15:49:59 +0900

    250912 스택 baekjoon

---
+++ src/baekjoon/silver/S4_10828/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.silver.S4_10828;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String order = st.nextToken();
+            if (order.equals("push")) {
+                if (st.hasMoreTokens()) stack.push(Integer.parseInt(st.nextToken()));
+                else System.out.println("error");
+            } else if (order.equals("pop")) {
+                if (stack.isEmpty()) System.out.println(-1);
+                else System.out.println(stack.pop());
+            } else if (order.equals("size")) {
+                System.out.println(stack.size());
+            } else if (order.equals("empty")) {
+                if (stack.isEmpty()) System.out.println(1);
+                else System.out.println(0);
+            } else if (order.equals("top")) {
+                if (stack.isEmpty()) System.out.println(-1);
+                else System.out.println(stack.peek());
+            }
+        }
+    }
+}
