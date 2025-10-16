4be051ddf5b4eaa5beff18498120b13d045dab79
K.Dongyoung <doyokim75@gmail.com>
2025-10-16 16:55:30 +0900

    251016 baekjoon.silver.S5_2941 크로아티아 알파벳

--- src/baekjoon/silver/S5_2941/Main.java
+++ src/baekjoon/silver/S5_2941/Main.java
@@ -11,8 +11,8 @@ public class Main {
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '=' || c == '-') answer--;
-            if (c == 'd' && i + 2 < s.length() && s.charAt(i + 1) == 'z' && s.charAt(i + 2) == '=') answer--;
-            if (c == 'j' && i - 1 >= 0 && (s.charAt(i - 1) == 'l' || s.charAt(i - 1) == 'n')) answer--;
+            else if (c == 'd' && i + 2 < s.length() && s.charAt(i + 1) == 'z' && s.charAt(i + 2) == '=') answer--;
+            else if (c == 'j' && i - 1 >= 0 && (s.charAt(i - 1) == 'l' || s.charAt(i - 1) == 'n')) answer--;
         }
 
         System.out.println(answer);
