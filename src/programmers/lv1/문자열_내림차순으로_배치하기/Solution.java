5e80b7359a15491ab594b38d532a9cf62f3862b5
K.Dongyoung <doyokim75@gmail.com>
2025-10-16 16:01:02 +0900

    251016 programmers.lv1.문자열_내림차순으로_배치하기

--- src/programmers/lv1/문자열_내림차순으로_배치하기/Solution.java
+++ src/programmers/lv1/문자열_내림차순으로_배치하기/Solution.java
@@ -17,10 +17,35 @@ public class Solution {
         return new StringBuilder(new String(charArray)).reverse().toString();
     }
 
+    public String solution3(String s) {
+        int[] upper = new int['Z' - 'A' + 1];
+        int[] lower = new int['z' - 'a' + 1];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'a') {
+                lower[c - 'a']++;
+            } else {
+                upper[c - 'A']++;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = lower.length - 1; i >= 0; i--) {
+            while (lower[i]-- > 0) {
+                sb.append((char) (i + 'a'));
+            }
+        }
+        for (int i = upper.length - 1; i >= 0; i--) {
+            while (upper[i]-- > 0) {
+                sb.append((char) (i + 'A'));
+            }
+        }
+        return sb.toString();
+    }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String answer = solution.solution2("Zbcdefg");
+        String answer = solution.solution3("Zbcdefg");
         System.out.println("answer = " + answer);
     }
 }
